5392db00461d570a0f189387d3bae0f834293890
Tyler Emanuel <tyleremanuel23@gmail.com>
2021-12-06 11:20:39 -0600

    Added Day 6 Part 1+2 Solutions

---
+++ 2021/day6/day6part1.py
@@ -0,0 +1,21 @@
+f = open('2021/day6/nums.txt', 'r')
+content = f.read()
+##print(content)
+
+updatedList = content.split(",")
+integer_map = map(int, updatedList)
+intList = list(integer_map)
+
+
+c = 0
+
+while c < 80:
+    for num in range(len(intList)):
+        if intList[num] == 0:
+            intList.append(8)
+            intList[num] = 6
+        else:
+            intList[num] -= 1
+    c += 1
+
+print(len(intList))
