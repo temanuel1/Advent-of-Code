6b276aa5c8c1a286e484850690971fbaa573615c
Tyler Emanuel <tyleremanuel23@gmail.com>
2021-12-09 13:28:42 -0600

    Updated Day 9 Part 1 Solution

--- 2021/day9/day9part1.py
+++ 2021/day9/day9part1.py
@@ -1,9 +1,9 @@
-f = open('2021/day9/test.txt', 'r')
+f = open('2021/day9/input.txt', 'r')
 content = f.read()
 numList = content
 lines = numList.split("\n")
 numsInLine = len(lines[0])
-print(numsInLine)
+#print(numsInLine)
 A = 1
 riskLevel = 0
 finalNumList = []
@@ -45,49 +45,51 @@ def rightDefaultLow(i):
 def default(i):
     return finalNumList[i] < finalNumList[i+1] and finalNumList[i] < finalNumList[i-1] and finalNumList[i] < finalNumList[i-numsInLine] and finalNumList[i] < finalNumList[i+numsInLine]
   
-print(len(finalNumList))
+#print(len(finalNumList))
 
 for i in range(len(finalNumList)):
     if isUp(i):
         if i == 0:
             if topLeftLow(i):
-                print("Low point at index: " + str(i))
+                #print("Low point at index: " + str(i))
                 riskLevel += (1+int(finalNumList[i]))
         elif i == (numsInLine - 1):
             if topRightLow(i):
-                print("Low point at index: " + str(i))
+                #print("Low point at index: " + str(i))
                 riskLevel += (1+int(finalNumList[i]))
         else:
             if topDefaultLow(i):
-                print("Low point at index: " + str(i))
+                #print("Low point at index: " + str(i))
                 riskLevel += (1+int(finalNumList[i]))
     elif isDown(i):
         if i % numsInLine == 0:
             if bottomLeftLow(i) :
-                print("Low point at index: " + str(i))
+                #print("Low point at index: " + str(i))
                 riskLevel += (1+int(finalNumList[i]))
         elif i == (len(finalNumList) - 1):
             if bottomRightLow(i):
-                print("Low point at index: " + str(i))
+                #print("Low point at index: " + str(i))
                 riskLevel += (1+int(finalNumList[i]))
         else:
             if bottomDefaultLow(i):
-                print("Low point at index: " + str(i))
+                #print("Low point at index: " + str(i))
                 riskLevel += (1+int(finalNumList[i]))
     else:
         if i % numsInLine == 0:
             if leftDefaultLow(i):              
-                print("Low point at index: " + str(i))
+                #print("Low point at index: " + str(i))
                 riskLevel += (1+int(finalNumList[i]))
-        elif i % (numsInLine - 1) == 0:
+        elif (i + 1) % 100 == 0:
+            #print(str(i) + " is divisible by " + str(numsInLine - 1))
+            #print("Num at i " + str(finalNumList[i]))
             #print(i)
             if rightDefaultLow(i):
-                print("Low point at index: " + str(i))
+                #print("Low point at index: " + str(i))
                 riskLevel += (1+int(finalNumList[i]))
         else:
             if default(i):
-                print("Low point at index: " + str(i))
+                #print("Low point at index: " + str(i))
                 riskLevel += (1+int(finalNumList[i]))
 
-#Should be 465
+
 print("Answer: " + str(riskLevel))
